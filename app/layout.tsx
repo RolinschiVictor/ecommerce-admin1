6aa53984eeab63d7b4609176c1588751f79e38f8
Rolinschi Victor <victor.rolinschi@isa.utm.md>
2025-09-09 22:17:32 +0300

    Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import {
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToastProvider } from "@/providers/toast-provider";
+import Navbar from "@/components/navbar";
 
 
 
@@ -28,30 +30,13 @@ export const metadata: Metadata = {
   description: "Admin Dashboard",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
+        <body>
+          <Navbar />
+          <ToastProvider />
           {children}
           <ModalProvider />
         </body>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -10,6 +10,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -27,10 +28,12 @@ export const StoreModal = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
             setLoading(true);
+
             const response = await axios.post('/api/stores', values);
+            window.location.assign(`/${response.data.id}`);
         }
         catch (error) {
-            console.log(error);
+        toast.error("Something went wrong");
         } finally {
             setLoading(false);
         }
