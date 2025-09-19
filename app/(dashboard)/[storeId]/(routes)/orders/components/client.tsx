9f516561844e075d8b76a0425f9a6143040b1f97
Rolinschi Victor <victor.rolinschi@isa.utm.md>
2025-09-19 13:49:25 +0300

    Corectarea erorilor

--- app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -1,23 +1,26 @@
-'use client';
+"use client"
 
-import { DataTable } from '@/components/ui/data.table';
-import {Heading }from '@/components/ui/heading';
-import { Separator } from '@/components/ui/separator';
-import { OrderColumn, columns } from './columns';
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns} from "./columns";
+import { DataTable } from "@/components/ui/data.table";
+
 
 interface OrderClientProps {
-  data: OrderColumn[];
+    data: OrderColumn[]
 }
-
-const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
-  return (
-    <>
-      <Heading title={`Orders | This Month (${data.length}) `} description="Manage Orders for your store" />
-
-      <Separator />
-      <DataTable searchKey="products" columns={columns} data={data} />
-    </>
-  );
-};
-
-export default OrderClient;
+export const OrderClient: React.FC<OrderClientProps> = ({
+    data
+}) => {
+    return (
+        <>
+            <Heading
+                title={`Orders(${data.length})`}
+                description="Manage orders for your store"
+            />
+            <Separator/>
+            <DataTable  searchKey="products" columns={columns} data={data} />
+        </>
+       
+    )
+}
